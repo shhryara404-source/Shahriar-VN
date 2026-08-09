b5be5189d181c805da92c3394ef357aa02fa5009
shhryara404-source <shhryara404@gmail.com>
2026-08-10 03:17:24 +0800

    Add global language direction fix to main branch

---
+++ lang-fix.js
@@ -0,0 +1,1 @@
+(()=>{const apply=()=>{const l=localStorage.getItem('svn-lang')||'fa';document.documentElement.lang=l;document.documentElement.dir=l==='fa'?'rtl':'ltr'};apply();document.addEventListener('click',e=>{if(e.target.closest('[data-action="lang"],[data-lang]'))setTimeout(apply,0)})})();
